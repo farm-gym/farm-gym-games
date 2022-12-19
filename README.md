97e92a79fbd07f782c9d701b3c1bbcf1cc792942
Timothee Mathieu <timothee.mathieu@inria.fr>
2022-12-19 13:49:59 +0100

    add register and rlberry example

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 Some gym games constructed with farm-gym. Compatible with stablebaselines3.
 
+```
+import farmgym_games
+import gym
+
+env = gym_make(id="Farm1-v0")
+```
 
--- farmgym_games/__init__.py
+++ farmgym_games/__init__.py
@@ -1,2 +1,15 @@
 from .game0_env import Farm0
 from .game1_env import Farm1
+import gym
+
+gym.envs.register(
+     id='Farm0-v0',
+     entry_point='farmgym_games.game0_env:Farm0',
+     max_episode_steps=365,
+)
+
+gym.envs.register(
+     id='Farm1-v0',
+     entry_point='farmgym_games.game1_env:Farm1',
+     max_episode_steps=365,
+)
