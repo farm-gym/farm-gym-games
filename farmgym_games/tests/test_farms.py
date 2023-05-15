7656093a4aac61c32b965237eed1bbebf9313b18
brahim <brahimdriss.bd@gmail.com>
2023-05-15 15:39:51 +0200

    Updated Farm0 and Interactive notebook

--- farmgym_games/tests/test_farms.py
+++ farmgym_games/tests/test_farms.py
@@ -1,10 +1,12 @@
 import pytest
-from rlberry.utils.check_gym_env import check_gym_env
-from farmgym_games import Farm0, Farm1
+#from rlberry.utils.check_gym_env import check_gym_env
+#from farmgym_games import Farm0
 from rlberry.agents import AgentWithSimplePolicy
 
+from farmgym_games.game_catalogue.game0_env import Farm0
 
-ALL_ENVS = [Farm0, Farm1]
+
+ALL_ENVS = [Farm0]
 
 # test gym compatibility
 
@@ -28,7 +30,7 @@ class RandomAgent(AgentWithSimplePolicy):
         episode_reward = 0
         for ep in range(int(budget)):
             action = self.policy(observation)
-            observation, reward, done, _ = self.env.step(action)
+            observation, reward, done, _, _ = self.env.step(action)
             episode_reward += reward
             if done:
                 self.writer.add_scalar("episode_rewards", episode_reward, ep)
@@ -42,5 +44,11 @@ class RandomAgent(AgentWithSimplePolicy):
 @pytest.mark.parametrize("Env", ALL_ENVS)
 def test_env_agent(Env):
     env = Env()
+    agent = RandomAgent(env)
+    agent.fit(10)
+
+if __name__ == "__main__":
+    env = Farm0()
+    #env.farm.understand_the_farm()
     agent = RandomAgent(env)
     agent.fit(10)
