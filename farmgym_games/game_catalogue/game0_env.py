12ccfb856ae59c381d9b0d25de8a15f9f3e29a09
brahim <brahimdriss.bd@gmail.com>
2023-05-15 17:52:18 +0200

    Update game0_env

--- farmgym_games/game_catalogue/game0_env.py
+++ farmgym_games/game_catalogue/game0_env.py
@@ -8,6 +8,12 @@ from farmgym_games.game_builder.utils import (
 )
 import numpy as np
 
+import os
+import cv2 
+import matplotlib.pyplot as plt
+import PIL.Image as Image
+
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 class Farm0(gym.Env):
     """
@@ -29,7 +35,7 @@ class Farm0(gym.Env):
     -----
     State:
         The state consists of
-    
+        ####
         * Day (from 1 to 365)
         * mean air temperature (°C)
         * min air temperature (°C)
@@ -44,14 +50,11 @@ class Farm0(gym.Env):
 
     Actions:
         The actions are :
-    
+        ####
         * doing nothing.
         * 2 levels of watering the field (1L or 5L of water)
         * harvesting
     """
-
-    name = "Farm0"
-
     observations_txt = [
         "Day (from 1 to 365)",
         "Mean air temperature (°C)",
@@ -61,86 +64,197 @@ class Farm0(gym.Env):
         "Sun-exposure (from 1 to 5)",
         "Consecutive dry day (int)",
         "Stage of growth of the plant",
+        "Number of fruits (int)",
         "Size of the plant in cm",
-        "nb of fruits",
+        "Soil wet_surface (m2.day-1)",
+        "fertilizer amount (kg)",
+        "Pollinators occurrence (bin)",
+        "Weeds grow (nb)",
+        "Weeds flowers (nb)",
         "weight of fruits",
+        "microlife health index (%)",
     ]
-
-    def __init__(self, api_compatibility=False):
+    name = "Farm0"
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps":4}
+    def __init__(self, render_mode = "rgb_array", api_compatibility = False):
         # init base classes
         gym.Env.__init__(self)
         self.api_compatibility = api_compatibility
 
         self.farm = cb.env()
-        self.farm.gym_step([])
-        # observation and action spaces
-        # Day, temp mean, temp min, temp max, rain amount, sun exposure, consecutive dry day, stage, size#cm, fruit weight, nb of fruits, weights
-        high = np.array([365, 50, 50, 50, 300, 7, 100, 10, 200, 100, 5000])
-        low = np.array([0, -50, -50, -50, 0, 0, 0, 0, 0, 0, 0])
-        self.n_obs = len(high)
-        self.observation_space = spaces.Box(low=low, high=high)
-        self.action_space = spaces.Discrete(4)
-
+        self.observation_space = self.farm.observation_space
+        self.action_space = self.farm.action_space
         # monitoring writer
         params = {}
         self.iteration = 0
 
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+        self.window = None
+        self.clock = None
+        
         # initialize
         self.state = None
         self.reset()
 
     def reset(self):
-        observation = self.farm.gym_reset()[0]
-        obs1, _, _, info = self.farm.gym_step([])
+        #### add back hide final state of plants
+        observation = self.farm.reset()[0]
         if self.api_compatibility:
-            return observation_hide_final_state_of_plants(
-                farmgymobs_to_obs(obs1), id_of_plants_stage=7
-            )
+            return observation
         else:
-            return observation_hide_final_state_of_plants(
-                farmgymobs_to_obs(obs1), id_of_plants_stage=7
-            ), {}
+            return observation, {}
 
     def step(self, action):
-        # Stepping
-        #   farmgym run with a cycle of 2 steps: 1 (empty) step of getting observation ("morning"), then 1 step of acting ("afternoon").
-        #   Classic RL methodology use only 1 step : performing an action return the next observation
-        #   To match this 2, we run the 'farmgy observation step ("morning")' right after the action.
-        #   With this method, it will be like classic RL 'step' for the user
-        _, reward, is_done, info = self.farm.farmgym_step(self.num_to_action(action))
-        obs1, _, _, info = self.farm.gym_step([])
-
-        observation = observation_hide_final_state_of_plants( farmgymobs_to_obs(obs1), id_of_plants_stage=7)
-
+        observation, reward, is_done, _, info = self.farm.step(action)        
+        #### Add back hide final state
+        ####observation, _, _, _,info = self.farm.step([])
+        ####observation = observation_hide_final_state_of_plants( farmgymobs_to_obs(obs1), id_of_plants_stage=7)
         if self.api_compatibility:
             return step_api_compatibility((observation, reward, False, is_done, info), output_truncation_bool=False)
         else:
             return observation, reward, False, is_done, info
 
-    def num_to_action(self, num):
-        if num == 1:
-            return [
-                (
-                    "BasicFarmer-0",
-                    "Field-0",
-                    "Soil-0",
-                    "water_discrete",
-                    {"plot": (0, 0), "amount#L": 1, "duration#min": 60},
-                )
-            ]
-        elif num == 2:
-            return [
-                (
-                    "BasicFarmer-0",
-                    "Field-0",
-                    "Soil-0",
-                    "water_discrete",
-                    {"plot": (0, 0), "amount#L": 5, "duration#min": 60},
-                )
-            ]
-        elif num == 3:
-            return [("BasicFarmer-0", "Field-0", "Plant-0", "harvest", {})]
+    def render(self):
+        image = self._render_frame()
+        if self.render_mode == "human":
+            cv2.imshow("Game", image)
+            cv2.waitKey(1000//self.metadata['render_fps'])
+        elif self.render_mode == "rgb_array":
+            return image
         else:
-            return []  # Do nothing.
+            raise ValueError("Unsupported rendering mode")
+    
+    def _render_frame(self):
+        max_display_actions = self.farm.rules.actions_allowed["params"]["max_action_schedule_size"]
 
+        from PIL import Image, ImageDraw, ImageFont
+        field = self.farm.fields["Field-0"]
+
+        im_width, im_height = 64, 64
+        XX = field.X+1
+        YY = (field.Y
+                + (int)(
+                    np.ceil(
+                        len(
+                            [1 for e in field.entities if field.entities[e].to_thumbnailimage() != None]
+                        )
+                        / field.X
+                    )
+                ))
+        font_size = im_width * XX // 10
+
+        offsetx = im_width // 2
+        offset_header = font_size * 2
+        offset_sep = font_size // 2
+        offset_foot = font_size * 2
+
+        font = ImageFont.truetype(str(CURRENT_DIR) + "/rendering/Gidole-Regular.ttf", size=font_size)
+        font_action = ImageFont.truetype(
+            str(CURRENT_DIR) + "/rendering/Gidole-Regular.ttf",
+            size=im_width * XX // 18,
+        )
+
+        
+        left, top, right, bottom = font_action.getbbox("A")
+        car_height = np.abs(top - bottom) * 1.33 
+        offset_actions = (int)(car_height * max_display_actions + 5 * im_height // 100)
+
+        dashboard_picture = Image.new(
+            "RGBA",
+            (
+                im_width * XX,
+                im_height * YY + offset_header + offset_sep + offset_foot + offset_actions,
+            ),
+            (255, 255, 255, 255),
+        )
+        d = ImageDraw.Draw(dashboard_picture)
+
+        day = (int)(field.entities["Weather-0"].variables["day#int365"].value)
+        day_string = "Day {:03d}".format(day)
+
+        d.text(
+            (
+                dashboard_picture.width // 2 - len(day_string) * font_size // 4,
+                im_height * YY + offset_header + offset_sep + offset_foot // 4 + offset_actions,
+            ),
+            day_string,
+            font=font,
+            fill="black",
+            #stroke_fill="black",
+        )
+
+        # offset_field=0
+        text = "Field"  # "F-"+fi[-1:]
+        left, top, right, bottom = font.getbbox(text)
+        width_text = (int)(np.abs(right - left))
+        d.text(
+            (
+                offsetx + (field.X) * im_width // 2 - width_text // 2,
+                offset_header // 4,
+            ),
+            text,
+            font=font,
+            fill="black",
+        )
+
+        index = 0
+        for e in field.entities:
+            image = field.entities[e].to_fieldimage()
+            dashboard_picture.paste(image, (offsetx, offset_header), image)
+
+            j = index // field.X
+            i = index - j * field.X
+            image_t = field.entities[e].to_thumbnailimage()
+            if image_t != None:
+                dd = ImageDraw.Draw(image_t)
+                xx = offsetx + i * im_width
+                yy = offset_header + field.Y * im_height + offset_sep + j * im_height
+                dashboard_picture.paste(image_t, (xx, yy), image_t)
+                index += 1
+
+        offset_field_y = (
+            offset_header
+            + field.Y * im_height
+            + offset_sep
+            + ((index - 1) // field.X + 1) * im_height
+        )
+        d.rectangle(
+            [
+                (offsetx, offset_field_y),
+                (
+                    offsetx + field.X * im_width,
+                    offset_field_y + offset_actions + im_width // 100,
+                ),
+            ],
+            fill=(255, 255, 255, 255),
+            outline=(0, 0, 0, 255),
+            width=im_width // 100,
+        )
+
+        nb_a = 0
+        if self.farm.last_farmgym_action:
+            for a in self.farm.last_farmgym_action:
+                fa_key, fi_key, entity_key, action_name, params = a
+                if a[1] == fi_key and nb_a <= max_display_actions:
+                    text = action_name
+                    # print("DISPLAY ACTION",action_name, params)
+                    if (type(params) == dict) and ("plot" in params.keys()):
+                        text += " " + str(params["plot"])
+                    xx_a = offsetx + im_width // 100
+                    yy_a = offset_field_y + nb_a * car_height + im_width // 100
+                    d.text(
+                        (xx_a, yy_a),
+                        text,
+                        font=font_action,
+                        fill="black",
+                    )
+                    nb_a += 1
+
+        offsetx += (field.X + 1) * im_width
+
+
+        return np.array(dashboard_picture)
+    def close(self):
+        cv2.destroyAllWindows()
 
