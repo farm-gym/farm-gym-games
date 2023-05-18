3e1e7d1900dc514acd9e55ea03649467335eab6e
Odalric-ambrym <odalric.maillard@inria.fr>
2023-05-18 18:42:44 +0200

    New farm

---
+++ farmgym_games/game_catalogue/farms_1x1/farm.py
@@ -0,0 +1,24 @@
+#from farmgym.v2.games.rungame import Farmgym_RandomAgent, run_gym_xp
+
+from farmgym_games.game_agents.basic_agents import Farmgym_RandomAgent
+from farmgym_games.game_builder.run_farm import run_gym_xp
+from farmgym_games.game_builder.make_farm import make_farm
+
+
+import os
+
+def env():
+    yaml_path = os.path.join(os.path.dirname(__file__),"farm_lille_clay_bean.yaml")
+    farm = make_farm(yaml_path)
+    # farm.add_monitoring(
+    #     make_variables_to_be_monitored(
+    #         [
+    #             "f0.soil.available_Water#L"
+    #         ]
+    #     )
+    # )
+    return farm
+
+if __name__ == "__main__":
+    agent = Farmgym_RandomAgent()
+    run_gym_xp(env(), agent, max_steps=15, render="text")
