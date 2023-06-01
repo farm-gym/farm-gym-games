d903c735d8aced50155a52d531853cdfc9eede5c
brahim <brahimdriss.bd@gmail.com>
2023-06-01 16:07:06 +0200

    Draft: Monitoring and policies examples

--- farmgym_games/game_builder/run_farm.py
+++ farmgym_games/game_builder/run_farm.py
@@ -38,3 +38,24 @@ def run_gym_xp(farm, agent, max_steps=np.infty, render=True, monitoring=False):
         farm.render()
         generate_video(image_folder=".", video_name="farm.avi")
         os.chdir("../")
+
+def run_policy_xp(farm, policy, max_steps=np.infty):
+    if farm.monitor != None:
+        farm.monitor = None
+    cumreward = 0.0
+    cumcost = 0.0
+    policy.reset()
+    observation = farm.reset()
+    terminated = False
+    i = 0
+    while (not terminated) and i <= max_steps:
+        observations = farm.get_free_observations()
+        observation_schedule = policy.observation_schedule(observations)
+        observation, _, _,_, info = farm.farmgym_step(observation_schedule)
+        obs_cost = info["observation cost"]
+        intervention_schedule = policy.intervention_schedule(observation)
+        obs, reward, terminated, truncated, info = farm.farmgym_step(intervention_schedule)
+        int_cost = info["intervention cost"]
+        cumreward += reward
+        cumcost += obs_cost + int_cost
+    return cumreward, cumcost
